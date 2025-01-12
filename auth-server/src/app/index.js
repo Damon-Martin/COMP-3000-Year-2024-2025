b79f0b9a41b33b2bf1db4958d05edcbf65b55ab5
Damon Martin <damon.martin@students.plymouth.ac.uk>
2025-01-12 14:58:37 +0000

    error handling bad req body

--- auth-server/src/app/index.js
+++ auth-server/src/app/index.js
@@ -4,6 +4,10 @@ import AuthRouter from './routes/auth-routes.js';
 const app = express();
 const port = 3000
 
+// Global Middlewares
+app.use(express.json());
+
+// Routing
 app.use("/v1", AuthRouter)
 
 app.listen(port, () => {
--- auth-server/src/app/routes/auth-routes.js
+++ auth-server/src/app/routes/auth-routes.js
@@ -3,7 +3,34 @@ import express from 'express'
 const AuthRouter = express.Router();
 
 AuthRouter.get("/register", (req, res) => {
-    res.send("/router endpoint");
+    const username = req.body.username;
+    const pass = req.body.password;
+
+    if (!username && !pass) {
+        return(
+            res.status(400).json({
+                error: "You are missing the username & password"
+            })
+        );
+    }
+    if (!username) {
+        return(
+            res.status(400).json({
+                error: "You are missing the username"
+            })
+        )
+    }
+    if (!pass) {
+        return(
+            res.status(400).json({
+                error: "You are missing the password"
+            })
+        )
+    }
+
+    res.status(200).json({
+        msg: "Saved user to db"
+    })
 })
 
 export default AuthRouter;
