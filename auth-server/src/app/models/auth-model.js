cb7126c43515a50e0fc98498ae16a67293f8914b
Damon Martin <damon.martin@students.plymouth.ac.uk>
2025-01-19 18:27:24 +0000

    made models

---
+++ auth-server/src/app/models/auth-model.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const AuthSchema = new Schema(
+  // passwords must be hashed and salted
+  {
+    username: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+  },
+  // Time stamping to help authorites for logging
+  {
+    timestamps: { createdAt: 'created_At', updatedAt: 'updated_At' }
+  }
+);
+
+export default mongoose.model("AuthModel", AuthSchema);
