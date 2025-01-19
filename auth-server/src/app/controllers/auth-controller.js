09bc2da828cee939939d7aa4ef567e2d5cff063f
Damon Martin <damon.martin@students.plymouth.ac.uk>
2025-01-19 18:51:12 +0000

    login endpoint

--- auth-server/src/app/controllers/auth-controller.js
+++ auth-server/src/app/controllers/auth-controller.js
@@ -3,7 +3,28 @@ class AuthController {
         this.AuthModel = AuthModel;
     }
 
-    login(username, password) {}
+    login(username, password) {
+        try {
+            const user = this.AuthModel.findOne(username);
+
+            if (user.password == password) {
+                return {
+                    code: 200,
+                }
+            }
+            return {
+                code: 401,
+                msg: "Username or Password Incorrect"
+            }
+        }
+        catch {
+            // Account does not exist
+            return {
+                code: 404,
+                msg: "Username or Password Incorrect"
+            }
+        }
+    }
 
     register(username, password) {}
 
