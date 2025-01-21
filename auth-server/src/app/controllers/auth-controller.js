e5a4af9e95e9e3eab5aee1be1731548b1c356453
Damon Martin <damon.martin@students.plymouth.ac.uk>
2025-01-21 18:45:56 +0000

    Sessions and JWT works for /registration

--- auth-server/src/app/controllers/auth-controller.js
+++ auth-server/src/app/controllers/auth-controller.js
@@ -1,15 +1,22 @@
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+
 import CredentialChecker from '../business-logic/credential-checker.js';
 
 class AuthController {
-    constructor(AuthModel) {
+    constructor(AuthModel, SessionModel) {
         this.AuthModel = AuthModel;
+        this.SessionModel = SessionModel;
         this.checker = new CredentialChecker();
     }
 
+    // Returns JWT and Makes new Session
     login(username, password) {
     }
 
-    async registerCustomer(username, password) {
+    // Returns JWT and Registers UName & Pass to DB
+    // For Customers Specifically
+    async register(username, password) {
         try{
             const checkerRes = this.checker.CheckRequestBody(username, password);
 
@@ -21,20 +28,44 @@ class AuthController {
                 }
             }
 
+            let encryptPass = await bcrypt.hash(password, 10)
+
             // Make New User
             const user = new this.AuthModel({
                 username: username,
-                password: password
+                password: encryptPass
             })
 
             try {
                 // Saving User
                 await user.save();
 
-                return {
-                    code: 200,
-                    msg: "Customer Registered Successfully"
+                let token = jwt.sign({ userId: user._id }, 'Example-Secret-Key', {
+                    expiresIn: '1h',
+                });
+
+                try {
+                    const sessionModel = new this.SessionModel({
+                        userId: user._id,
+                        token: token,
+                        expiresAt: Date.now() + 3600 * 1000, // Token Expiration (1 hour)
+                    });
+    
+                    sessionModel.save();
+    
+                    return {
+                        code: 200,
+                        token: token,
+                        msg: "Customer Registered Successfully"
+                    }
                 }
+                catch(e) {
+                    return {
+                        code: 500,
+                        error: `Username + Encrypted Password saved to db but failed creating a new session: ${e}`
+                    }
+                }
+
             }
             // User Already Exists
             catch(e) {
@@ -53,8 +84,6 @@ class AuthController {
             }
         }
     }
-
-    async registerAdmin(username, password) {}
 
     validateJWT(token) {}
 
--- auth-server/src/app/routes/auth-routes.js
+++ auth-server/src/app/routes/auth-routes.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import AuthController from '../controllers/auth-controller.js';
+
+// DB Models
 import AuthModel from '../models/auth-model.js';
+import SessionModel from '../models/session-model.js'
 
 const AuthRouter = express.Router();
 
-const authController = new AuthController(AuthModel);
+const authController = new AuthController(AuthModel, SessionModel);
 
 
 /**
@@ -45,12 +48,12 @@ const authController = new AuthController(AuthModel);
 AuthRouter.get("/register", async (req, res) => {
     const uName = req.body.username;
     const pass = req.body.password;
-    // If there is a uName & pass (logic below)
 
     const result = await authController.register(uName, pass);
 
     if (result.code == 200) {
         res.status(result.code).json({
+            token: result.token,
             msg: result.msg
         })
     }
