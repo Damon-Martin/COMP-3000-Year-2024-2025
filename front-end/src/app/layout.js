add323ed587a23134b7f1a49bf13b8711b621bd3
Damon Martin <damon.martin@students.plymouth.ac.uk>
2025-01-22 14:49:54 +0000

    front-end added (Worked on Later)

---
+++ front-end/src/app/layout.js
@@ -0,0 +1,27 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "El Comercio",
+  description: "A full stack E-Commerce application guided with HCI feedback and software validation",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
