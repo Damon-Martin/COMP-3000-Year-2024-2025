fb3f65e2604f83f3dbbffcb6061bc14ac58b6082
Damon Martin <damon.martin@students.plymouth.ac.uk>
2025-01-22 19:10:02 +0000

    setting up env for security

--- back-end/src/app/index.js
+++ back-end/src/app/index.js
@@ -2,10 +2,11 @@ import express from 'express'
 import ItemRouter from '../routes/item-routes.js'
 
 const app = express()
+const isProduction = process.env.PRODUCTION;
 const port = 3000
 
 app.use("/v1", ItemRouter);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port} & Production is ${isProduction}`)
 })
