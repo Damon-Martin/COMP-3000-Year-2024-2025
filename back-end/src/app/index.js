189128155e56b8369233b66166b74ed7059bc2c1
Damon Martin <damon.martin@students.plymouth.ac.uk>
2025-01-22 21:49:41 +0000

    Swagger and Starting Item Controller

--- back-end/src/app/index.js
+++ back-end/src/app/index.js
@@ -1,9 +1,16 @@
 import express from 'express'
-import ItemRouter from '../routes/item-routes.js'
+import ItemRouter from './routes/item-routes.js'
+
+import { specs, swaggerUI } from './config/swagger-config.js';
 
 const app = express()
-const isProduction = process.env.PRODUCTION;
+const isProduction = process.env.PRODUCTION === 'true'; // Using Logic to determine if true or false based on env string
 const port = 3000
+
+// Display swagger if in development
+if (!isProduction) {
+  app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+}
 
 app.use("/v1", ItemRouter);
 
