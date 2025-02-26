ca8cf9a266d034192fb22dd2832d717cc19dab1f
Damon Martin <damon.martin@students.plymouth.ac.uk>
2025-02-26 17:29:21 +0000

    Front End Layout

---
+++ front-end/src/app/page.jsx
@@ -0,0 +1,15 @@
+import HeaderBar from "@/components/all-pages/header-bar/header-bar";
+import DesktopNavBar from "@/components/nav-bar/desktop/nav-desktop";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <HeaderBar/>
+      <DesktopNavBar/>
+      <main>
+        asdfsdafasd
+      </main>
+    </div>
+  );
+}
