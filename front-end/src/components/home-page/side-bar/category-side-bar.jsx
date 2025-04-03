10531cf47573287980481b63eafffa677873f61b
Damon Martin <damon.martin@students.plymouth.ac.uk>
2025-04-03 09:25:45 +0100

    Fixed UI Bug

--- front-end/src/components/home-page/side-bar/category-side-bar.jsx
+++ front-end/src/components/home-page/side-bar/category-side-bar.jsx
@@ -1,6 +1,7 @@
 export default function CategorySideBar() {
-    return (
-      <div className="bg-[#FF4D00] h-[80dvh] w-[15dvw]">
+  return (
+      <div className="bg-[#FF4D00] h-[80dvh] w-[15dvw] flex items-center justify-center">
+          <h3 className="text-white">Categories</h3>
       </div>
-    );
+  );
 }
