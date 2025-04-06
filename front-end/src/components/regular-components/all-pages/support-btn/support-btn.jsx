c627f265bead503febeedd9ff8790e9cd4d44a6c
Damon Martin <damon.martin@students.plymouth.ac.uk>
2025-04-07 00:16:25 +0100

    Keyboard navigation improvement

--- front-end/src/components/regular-components/all-pages/support-btn/support-btn.jsx
+++ front-end/src/components/regular-components/all-pages/support-btn/support-btn.jsx
@@ -3,18 +3,17 @@ import Image from 'next/image';
 
 export default function SupportButton() {
     return (
-        <Link href="/login">
-            <div
-                className="fixed bottom-4 right-4 flex items-center justify-center bg-blue-500 text-white rounded-full shadow-md hover:bg-blue-600 transition duration-300"
-                style={{ width: "10dvh", height: "10dvh" }}
-                tabIndex={0}
-            >
-                <Image 
-                    src="/images/support-icon/support-icon.svg" 
-                    width={40}
-                    alt="Support Icon" 
-                />
-            </div>
+        <Link
+            href="/login"
+            className="fixed bottom-4 right-4 flex items-center justify-center bg-blue-500 text-white rounded-full shadow-md hover:bg-blue-600 transition duration-300"
+            style={{ width: "10dvh", height: "10dvh" }}
+            aria-label="Support Page"
+        >
+            <Image 
+                src="/images/support-icon/support-icon.svg" 
+                width={40}
+                alt="Support Icon" 
+            />
         </Link>
     );
 }
