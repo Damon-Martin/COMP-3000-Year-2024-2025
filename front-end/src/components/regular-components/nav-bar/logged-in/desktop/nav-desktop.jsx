8e540e5f79f58b47b6d3b4007a454591a5578309
Damon Martin <damon.martin@students.plymouth.ac.uk>
2025-04-06 19:15:49 +0100

    Active Icons Nav Bar (UX Functionality)

--- front-end/src/components/regular-components/nav-bar/logged-in/desktop/nav-desktop.jsx
+++ front-end/src/components/regular-components/nav-bar/logged-in/desktop/nav-desktop.jsx
@@ -7,10 +7,10 @@ export default function DesktopNavBar() {
         <div className="bg-[#D9D9D9] text-black h-[5dvh] w-full flex items-center justify-between">
             {/* Right-aligned Links */}
             <div className="ml-auto flex items-center">
-                <Link href="/" className="mr-3 hover:text-[#FF4D00]">Home</Link>
-                <Link href="/account" className="mr-3 hover:text-[#FF4D00]">Account</Link>
-                <Link href="/settings" className="mr-3 hover:text-[#FF4D00]">Settings</Link>
-                <Link href="/" className="mr-3 hover:text-[#FF4D00]">Basket</Link>
+                <NavBtn size={24} redirectURL="/" altTxt="Home Page" svgLocation="/images/shop-icon/shop-icon.svg" activeSvgLocation="/images/shop-icon/shop-active.svg"  />
+                <NavBtn size={24} redirectURL="/" altTxt="Account Page" svgLocation="/images/account-icon/account-icon.svg" activeSvgLocation="/images/account-icon/account-active.svg" />
+                <NavBtn size={24} redirectURL="/settings" altTxt="Settings Page" svgLocation="/images/settings-icon/settings-icon.svg" activeSvgLocation="/images/settings-icon/settings-active.svg"  />
+                <NavBtn size={24} redirectURL="/" altTxt="Basket Page" svgLocation="/images/basket-icon/basket-icon.svg" activeSvgLocation="/images/basket-icon/basket-active.svg"  />
             </div>
         </div>
     );
