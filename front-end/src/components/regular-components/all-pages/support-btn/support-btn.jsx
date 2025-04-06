90677d600cdcda7f30689a3aa722666546893df6
Damon Martin <damon.martin@students.plymouth.ac.uk>
2025-04-07 00:04:58 +0100

    Fixed Link Components Larger

--- front-end/src/components/regular-components/all-pages/support-btn/support-btn.jsx
+++ front-end/src/components/regular-components/all-pages/support-btn/support-btn.jsx
@@ -7,6 +7,7 @@ export default function SupportButton() {
             <div
                 className="fixed bottom-4 right-4 flex items-center justify-center bg-blue-500 text-white rounded-full shadow-md hover:bg-blue-600 transition duration-300"
                 style={{ width: "10dvh", height: "10dvh" }}
+                tabIndex={0}
             >
                 <Image 
                     src="/images/support-icon/support-icon.svg" 
--- front-end/src/components/regular-components/home-page/side-bar/category-side-bar.jsx
+++ front-end/src/components/regular-components/home-page/side-bar/category-side-bar.jsx
@@ -1,7 +1,11 @@
+import Link from 'next/link'
+
 export default function CategorySideBar() {
-  return (
-      <div className="bg-[#FF4D00] h-[80dvh] w-[15dvw] flex items-center justify-center">
-          <h3 className="text-white">Categories</h3>
-      </div>
+    return (
+        <Link href="/login">
+            <div className="bg-[#FF4D00] h-[80dvh] w-[15dvw] flex items-center justify-center">
+                <h3 className="text-white">Categories</h3>
+            </div>
+        </Link>
   );
 }
