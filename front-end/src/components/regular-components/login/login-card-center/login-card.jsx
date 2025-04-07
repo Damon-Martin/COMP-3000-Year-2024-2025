2725cf1d3fe34c51472ae2e7088b18a4ac419264
Damon Martin <damon.martin@students.plymouth.ac.uk>
2025-04-07 17:33:13 +0100

    aligning items better

--- front-end/src/components/regular-components/login/login-card-center/login-card.jsx
+++ front-end/src/components/regular-components/login/login-card-center/login-card.jsx
@@ -12,18 +12,17 @@ export default function LoginCard({
     marginSides = "mx-10",  
     marginTop = "mt-10"}) {
 
-
-
     return (
         <form 
-            className={`text-black ${width} ${minWidth} ${maxWidth} ${height} ${minHeight} ${maxHeight} ${marginTop} md:${marginSides}`}
+            className={`text-black ${width} ${minWidth} ${maxWidth} ${height} ${minHeight} ${maxHeight} ${marginTop} md:${marginSides} flex items-center justify-center`}
             style={{ backgroundColor: color }}  
         >
-            <p>Login</p>
-            <input type="text" placeholder="username"></input>
-            <input type="password" placeholder="password"></input>
-            <LoginBtnRow />
+            <div className="text-center">  {/* Added this div to center text inside the form */}
+                <p>Login</p>
+                <input type="text" placeholder="username" className="m-1"></input> <br/>
+                <input type="password" placeholder="password" className="m-1"></input>
+                <LoginBtnRow />
+            </div>
         </form>
     );
 }
-
