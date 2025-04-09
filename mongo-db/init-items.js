d0d1b59a7254d201d885004eab9bcffeb94e4730
Damon Martin <damon.martin@students.plymouth.ac.uk>
2025-04-09 17:42:41 +0100

    Seeding Format Established part 1

---
+++ mongo-db/init-items.js
@@ -0,0 +1,37 @@
+db = db.getSiblingDB('database'); // Selecting the DB
+
+// Ensuring there is at least 1 patient during initialization
+db.items.insertMany([
+    {
+        "_id":  ObjectId('67f6a1b7c02fe16921544ca7'),
+        "name": "Leather Jacket",
+        "description": "A stylish leather jacket perfect for all seasons.",
+        "price": 29.99,
+        "totalSold": 0,
+        "imageUrl": "https://cdn.pixabay.com/photo/2014/08/26/21/49/jackets-428622_1280.jpg",
+        "altImgTxt": "Image of a leather jacket",
+        "createdAt": {
+          "$date": "2025-04-09T16:13:47.296Z"
+        },
+        "updatedAt": {
+          "$date": "2025-04-09T16:13:47.296Z"
+        },
+        "__v": 0
+    },
+    {
+        "_id": ObjectId('67f6a1b7c02fe16921544ca8'),
+        "name": "Rain Coat",
+        "description": "A rain coat which is designed to withstand the British Weather and Climate during storms",
+        "price": 99.99,
+        "totalSold": 0,
+        "imageUrl": "https://cdn.pixabay.com/photo/2014/08/26/21/49/jackets-428622_1280.jpg",
+        "altImgTxt": "Image of a Rain Coat",
+        "createdAt": {
+          "$date": "2025-04-09T16:13:47.296Z"
+        },
+        "updatedAt": {
+          "$date": "2025-04-09T16:13:47.296Z"
+        },
+        "__v": 0
+    }
+]);
